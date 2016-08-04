f4dc3396d54679a14a66b71b2369ce7a1278ce81
Glenn Gartner <glenngartner@gmail.com>
2016-08-03 22:51:28 -0400

    Updload prior to OS upgrade. Backup to github

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 #use devise
 gem 'devise'
 
+# Use figaro to manage passwords
+gem 'figaro', '1.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # Asset digests allow you to set far-future HTssed.
+  config.assets.compile = false
+TPTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
