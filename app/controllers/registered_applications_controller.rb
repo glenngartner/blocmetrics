bcc72a91d361f1f79aac79f497e40629dbb7db91
Glenn Gartner <glenngartner@gmail.com>
2016-08-11 01:31:22 -0400

    Add edit and delete actions and views

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -21,16 +21,39 @@ class RegisteredApplicationsController < ApplicationController
     end
 
     def edit
+        @reg_app = RegisteredApplication.find(params[:id])
     end
 
     def update
+        @reg_app = RegisteredApplication.find(params[:id])
+        @reg_app.name = params[:registered_application][:name]
+        @reg_app.url = params[:registered_application][:url]
+        @reg_app.user_id = current_user.id
+
+        if @reg_app.save
+            flash[:notice] = 'App was updated successfully'
+            redirect_to registered_applications_path
+
+        else
+
+            flash.now[:alert] = 'There was an error saving the app. Please try again.'
+            render :new
+        end
     end
 
     def destroy
+        @reg_app = RegisteredApplication.find(params[:id])
+        if @reg_app.destroy
+            flash[:notice] = "\"#{@reg_app.name}\" was deleted successfully."
+            redirect_to registered_applications_path
+        else
+            flash.now[:alert] = 'There was an error deleting the app.'
+            render :show
+        end
     end
 
     def show
-      @reg_app = RegisteredApplication.find(params[:id])
+        @reg_app = RegisteredApplication.find(params[:id])
     end
 
     def index
